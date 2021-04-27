8f9f21824045ff31e6fb83d86100ae3c6da50e2e
richardingwe <richardingwe5@gmail.com>
2021-04-27 23:11:00 +0100

    styled header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
-	<header>
-		<div>Covid-19 Tracker</div>
+	<header class="text-center bg-blue-800 text-white p-4 mb-10">
+		<div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+		<p>
+			API by
+			<a
+				href="https://covid19api.com"
+				target="_blank"
+				class="text-blue-300"
+			></a>
+		</p>
 	</header>
 </template>
 
