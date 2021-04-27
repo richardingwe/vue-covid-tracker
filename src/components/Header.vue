0a54296862cdd58f7a181ae856fcc7020a886113
richardingwe <richardingwe5@gmail.com>
2021-04-27 23:16:53 +0100

    added virus icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
 	<header class="text-center bg-blue-800 text-white p-4 mb-10">
-		<div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+		<div class="text-3xl md:text-5xl font-bold mb-3">
+			<i class="fa fa-viruses" /> Covid-19 Tracker
+		</div>
 		<p>
 			API by
-			<a
-				href="https://covid19api.com"
-				target="_blank"
-				class="text-blue-300"
-			></a>
+			<a href="https://covid19api.com" target="_blank" class="text-blue-300"
+				>covid19api.com</a
+			>
 		</p>
 	</header>
 </template>
