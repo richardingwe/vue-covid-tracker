31ecf4efeddc8ffb8928f89b2ddb3d7fc3c0878f
richardingwe <richardingwe5@gmail.com>
2021-04-28 13:23:18 +0100

    added the hourglass to assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,5 +8,26 @@
 	export default {
 		name: 'Home',
 		components: {},
+		data() {
+			return {
+				loading: true,
+				title: 'Global',
+				dataDate: '',
+				stats: {},
+				countries: [],
+				loadingImage: require('../assets/hourglass.gif'),
+			};
+		},
+		methods: {
+			async fetchCovidData() {
+				const res = await fetch('https://api.covid19api.com/summary');
+				const data = await res.json();
+				return data;
+			},
+		},
+		async created() {
+			const data = await this.fetchCovidData();
+			console.log(data);
+		},
 	};
 </script>
