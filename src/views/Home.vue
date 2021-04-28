30c1c7ecc40aea9530c02545b5192035dd02bf5b
richardingwe <richardingwe5@gmail.com>
2021-04-28 13:31:41 +0100

    created loading tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
+/* eslint-disable prettier/prettier */ /* eslint-disable prettier/prettier */
 <template>
-	<div class="home">hello world</div>
+	<main v-if="!loading">Show Data</main>
+	<main v-else class="flex flex-col align-center justify-center text-center">
+		<div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+		<img :src="loadingImage" alt="" srcset="" class="w-24 m-auto" />
+	</main>
 </template>
 
 <script>
@@ -27,7 +32,11 @@
 		},
 		async created() {
 			const data = await this.fetchCovidData();
-			console.log(data);
+
+			this.dataDate = data.Date;
+			this.stats = data.Global;
+			this.countries = data.Countries;
+			this.loading = false;
 		},
 	};
 </script>
